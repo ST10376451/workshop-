dcd2170b6465404c04d365b6887fc498a913b8bf
st10376451 <st10376451@vcconnect.edu.za>
2024-04-11 09:57:30 +0200

    pushing to workshop

---
+++ WorkshopProject/src/workshopproject/WorkshopProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package workshopproject;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class WorkshopProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
